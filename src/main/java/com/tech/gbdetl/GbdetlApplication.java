a096284efa7b59faadaf9b1c92082dd578927879
ComStrong <18406706068@163.com>
2018-11-21 22:24:27 +0800

    新项目

---
+++ src/main/java/com/tech/gbdetl/GbdetlApplication.java
@@ -0,0 +1,12 @@
+package com.tech.gbdetl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbdetlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbdetlApplication.class, args);
+    }
+}
